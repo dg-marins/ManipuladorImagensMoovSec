fc84c13e3f11f89cdcf4002b81b8e7a4a825c8e7
Douglas Marins <douglasmarinssouza@gmail.com>
2023-08-10 15:01:53 -0300

    Listagem de imagens e adição ao banco de dados adicionado

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from itertools import chain
 from consumer.consumer import Consumer
 from dto.banco import Database
 import os
 import json
+import datetime
 
 class Main():
 
@@ -32,23 +34,39 @@ class Main():
 
         #Inicia Database
         db = Database()
-        db.adicionar_carro('15204')
-        db.adicionar_info_carro(1, '2023-08-08T00:30:01Z', '2023-08-08T00:45:01Z', '1', '-03:00', '/BackupMedia/15204/hist-44D1AB4A721A5FD8-1.mp4', 'NO', '/BackupMedia/15204', '/home/publico/imagens/15204/camera1/2023-08-08')
+        # db.adicionar_info_carro(1, '2023-08-08T00:30:01Z', '2023-08-08T00:45:01Z', '1', '-03:00', 'hist-33D8AF9D8702BF88-1.mp4', 'NO', '/BackupMedia/15204', '/home/publico/imagens/15204/camera1/2023-08-08')
         
-        
-        tc = db.carros_processados()
-        for carro_info in tc:
-            print(carro_info)
-
         cars = os.listdir(config_data.get("default_directory"))
 
+        #Gera uma lista com as datas dos ultimos 4 dias
+        dates = []
+        for i in range(10, -1, -1):
+            date = (datetime.datetime.now() - datetime.timedelta(days=i)).strftime('%Y-%m-%d')
+            dates.append(date)
+
+
         for car in cars:
-            date = '2023-08-07'
-            media_records = api_consumer.get_media_records(car, date, 
-                        config_data.get("source_records"), config_data.get("stream_type"))
 
-            # print(media_records)
+            db.adicionar_carro(car)
+            car_id = db.get_car_id_by_name(car)
 
+            source_car_path = os.path.join(config_data.get("default_directory"), car)
+            car_files = os.listdir(source_car_path)
+            
+            api_media_records = []
+            
+            for date in dates:
+                api_media_records.append(api_consumer.get_media_records(car, date, 
+                            config_data.get("source_records"), config_data.get("stream_type")))
+            
+            unified_api_media_records = list(chain(*api_media_records))
+
+            for file in car_files:
+                for info in unified_api_media_records:
+                    file_name = os.path.basename(info['fileName'])
+                    if file == file_name:
+                        db.adicionar_info_carro(car_id, info['starttime'], info['endtime'], info['channel'], 
+                                                info['timezone'], file_name, 'NO', os.path.dirname(info['fileName']), config_data.get('destination_directory'))
         db.close_connection()
 
 
