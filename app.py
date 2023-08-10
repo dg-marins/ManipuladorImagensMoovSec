be2add1931f11ddf1fca15fc6c7982ef655adcbd
Douglas Marins <douglasmarinssouza@gmail.com>
2023-08-10 10:41:56 -0300

    Atualização não relacionada

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from consumer.consumer import Consumer
+from dto.banco import Database
 import os
 import json
 
 class Main():
 
     def __init__(self) -> None:
-        pass
+        self._source_path = os.path.dirname(os.path.realpath(__file__))
 
 
     def ReadJson(self):
-
-        self._source_path = os.path.dirname(os.path.realpath(__file__))
         
         self._config_folder = os.path.join(self._source_path, 'config')
 
@@ -24,16 +23,34 @@ class Main():
 
     def main(self):
 
-        cd = self.ReadJson()
+        #Carrega arquivo de configuração
+        config_data = self.ReadJson()
 
-        car = '15204'
-        date = '2023-08-07'
+        #Configura Api Coonsumer
+        api_consumer = Consumer(config_data.get("host_ip"), config_data.get("host_port"), 
+                        config_data.get("user"), config_data.get("password"))
 
-        cc = Consumer(cd.get("host_ip"), cd.get("host_port"), cd.get("user"), cd.get("password"))
+        #Inicia Database
+        db = Database()
+        db.adicionar_carro('15204')
+        db.adicionar_info_carro(1, '2023-08-08T00:30:01Z', '2023-08-08T00:45:01Z', '1', '-03:00', '/BackupMedia/15204/hist-44D1AB4A721A5FD8-1.mp4', 'NO', '/BackupMedia/15204', '/home/publico/imagens/15204/camera1/2023-08-08')
+        
+        
+        tc = db.carros_processados()
+        for carro_info in tc:
+            print(carro_info)
 
-        media_records = cc.get_media_records(car, date, cd.get("source_records"), cd.get("stream_type"))
+        cars = os.listdir(config_data.get("default_directory"))
 
-        print(media_records)
+        for car in cars:
+            date = '2023-08-07'
+            media_records = api_consumer.get_media_records(car, date, 
+                        config_data.get("source_records"), config_data.get("stream_type"))
+
+            # print(media_records)
+
+        db.close_connection()
+
 
 if __name__ == '__main__':
     mr = Main()
---
+++ dto/banco.py
@@ -0,0 +1,68 @@
+import sqlite3
+import os
+
+class Database():
+    # Conectar-se ao banco de dados (ou criar um novo se não existir)
+
+    def __init__(self) -> None:
+
+        source_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "carros.db")
+        self.conn = sqlite3.connect(source_path)
+        self.cursor = self.conn.cursor()
+
+        # Criar a tabela de carros
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS cars (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                car TEXT UNIQUE
+            )
+        ''')
+
+        # Criar a tabela de informações dos carros
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS info_carros (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                carro_id INTEGER,
+                starttime TEXT,
+                finaltime TEXT,
+                channel TEXT,
+                timezone TEXT,
+                filename TEXT,
+                processed TEXT,
+                inicialpath TEXT,
+                finalpath TEXT,
+                FOREIGN KEY (carro_id) REFERENCES carros (id)
+            )
+        ''')
+
+    # Função para adicionar um carro à tabela 'carros'
+    def adicionar_carro(self, car):
+        try:
+            self.cursor.execute('''
+                INSERT INTO cars (car)
+                VALUES (?)
+            ''', (car,))
+            self.conn.commit()
+        except sqlite3.IntegrityError:
+            print(f"Esse carro {car} ja existe na tabela.")
+
+    # Função para adicionar informações de um carro à tabela 'info_carros'
+    def adicionar_info_carro(self, carro_id, starttime, finaltime, channel, timezone, filename, processed, inicialpath, finalpath):
+        self.cursor.execute('''
+            INSERT INTO info_carros (carro_id, starttime, finaltime, channel, timezone, filename, processed, inicialpath, finalpath)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (carro_id, starttime, finaltime, channel, timezone, filename, processed, inicialpath, finalpath))
+        self.conn.commit()
+
+    # Consulta para buscar carros processados com suas informações
+    def carros_processados(self, processed = 'NO'):
+        self.cursor.execute(f'''
+            SELECT ic.carro_id, c.car, ic.starttime, ic.finaltime, ic.channel, ic.timezone, ic.filename, ic.processed, ic.inicialpath, ic.finalpath
+            FROM cars c
+            JOIN info_carros ic ON c.id = ic.carro_id
+            WHERE ic.processed = '{processed}'
+        ''')
+        return self.cursor.fetchall()
+
+    def close_connection(self):
+        self.conn.close()
